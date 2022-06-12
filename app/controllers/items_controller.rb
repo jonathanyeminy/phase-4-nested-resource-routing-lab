4f1f4d9eb782196db8b6f0d7c7628ff3f5aafd48
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-06-12 14:35:25 -0400

    commit tree adc901a8f009ca147a67c6acad865c25e881f421

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,46 @@
 class ItemsController < ApplicationController
-
+  rescue_from   ActiveRecord::RecordNotFound, with: :throw_not_found_error
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = find_user
+      items = user.items
+      render json: items, include: :user
+    else
+      render json: Item.all, include: :user
+    end
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item
+   else
+    render json: {error: "404 Not Found"}, status: :not_found
+   end
+
+  end
+
+  def create
+    new_item = Item.create(item_params)
+    if new_item
+      render json: new_item, status: :created
+   else
+    render json: {error: "Unprocessable entity"}, status: :not_found
+   end
+  end
+  
+
+  private
+  def find_user
+    User.find(params[:user_id])
+  end
+  
+  def throw_not_found_error(exception)
+      render json: {error: "#{exception.model} not found"}, status: :not_found
+    end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
+  def index
+    users = User.all
+    render json: users, include: :items
+  end
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
-  end
+   end
 
 end
